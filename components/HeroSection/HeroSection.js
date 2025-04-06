bb3516274da6483d3cc4604bd273c9f9b3aa94aa
Basit-dysol <basit@dysols.com>
2025-04-06 22:08:42 +0300

    image size reduced

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -8,14 +8,15 @@ export default function HeroSection() {
 
   return (
     <>
-      <section className="flex flex-col -translate-y-16 text-white/90 items-baseline justify-center bg-[url('/LandingPage.png')] bg-cover text-center pt-28 rounded-b-4xl pb-14 px-20 space-y-10 pr-0 lg:pr-[45vw]">
-        <h1 className="text-7xl text-left font-medium pt-28">
-          Let’s take your idea from concept to market
+      <section className="flex flex-col h-[130vh] -translate-y-16 text-white/90 items-baseline justify-end bg-[url('/LandingPage.png')] bg-cover text-center rounded-b-4xl pb-[18vh] px-20 space-y-10 pr-0 lg:pr-[45vw]">
+        <h1 className="text-[clamp(2.5rem,3.5vw,7rem)] text-left font-medium pt-28">
+          Let's take your idea from concept to market
         </h1>
-        <p className="mt-4 items-start text-left text-lg">
+        <p className="mt-4 text-left text-[clamp(1rem,1.25vw,2.5rem)] leading-relaxed max-w-3xl">
           At Dysol, we bring your product idea to life with top-tier engineering
-          and design. From idea to manufacturing, we’re your trusted partner in
-          end-to-end product development. <br />
+          and design. From idea to manufacturing, we're your trusted partner in
+          end-to-end product development.
+          <br className="hidden sm:block" />
           With an expert team of engineers, technologists and programmers ready
           to help your business make an impact. Are you ready to pioneer the
           future?
