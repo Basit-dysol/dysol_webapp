4c7677a5c4dc54d0c5016ed824497c093f7d304c
Basit-dysol <basit@dysols.com>
2025-04-26 13:10:33 +0300

    max w removed

--- components/Container.js
+++ components/Container.js
@@ -1,6 +1,6 @@
 export const Container = ({ className, ...props }) => (
   <div
     {...props}
-    className={`w-full max-w-[1728px] bg-[#1A1A1A] mx-auto ${className || ''}`}
+    className={`w-full bg-[#1A1A1A] mx-auto ${className || ''}`}
   />
 );
