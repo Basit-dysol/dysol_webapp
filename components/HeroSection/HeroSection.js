5b34d0015b25dccb59723a1fd1d522552127307c
Amnah Razzaki <amnahrazzaki@gmail.com>
2025-05-04 13:47:54 +0500

    fixd border & font issue

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -15,15 +15,15 @@ export default function HeroSection() {
           Let's take your idea from concept to market
         </h1>
         <p className="text-lg lg:text-xl text-white/60 font-light lg:w-3xl text-left leading-relaxed"></p> */}
-      <section className="flex flex-col h-[50vw] m-2 xl:h-[60vw] 2xl:h-[65vw] text-white justify-end bg-[url('/LandingPage.png')] bg-cover bg-center  rounded-4xl pb-16 xl:px-24 2xl:px-32 space-y-6 pr-0 xl:pr-[40vw] 2xl:pr-[40vw]">
-        <h1 className="text-4xl xl:text-[4.25rem] 2xl:text-[5.125rem]  xl:w-[45vw] 2xl:w-[43vw] text-left font-medium xl:leading-[5.25rem] 2xl:leading-24">
+      <section className="flex flex-col w-full min-h-[calc(100vh-16px)] text-white justify-end bg-[url('/LandingPage.png')] bg-cover bg-center  rounded-4xl pb-16 xl:px-24 mx-2 xl:aspect-[156/100] 2xl:px-32 space-y-6 pr-0 xl:pr-[40vw] 2xl:pr-[40vw]">
+        <h1 className="text-4xl xl:text-[4.25rem] 2xl:text-[5.125rem]  xl:w-[45vw] 2xl:w-[43vw] text-left font-medium xl:leading-[5rem] 2xl:leading-24">
           Let's take your
           <br className="hidden sm:block" />
           idea from concept
           <br className="hidden sm:block" />
           to market
         </h1>
-        <p className="xl:text-shadow-md 2xl:text-xl xl:w-[42vw] 2xl:w-[40vw] text-white/60 font-extralight text-left leading-relaxed">
+        <p className="xl:text-shadow-md 2xl:text-xl xl:w-[42vw] 2xl:w-[40vw] text-white/60 font-extralight text-left leading-relaxed xl:leading-[24px]">
           At Dysol, we bring your product idea to life with top-tier engineering
           and design. From idea to manufacturing, we're your trusted partner in
           end-to-end product development.
