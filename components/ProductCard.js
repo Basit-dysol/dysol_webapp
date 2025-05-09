4b457ccf13a634ba1d70c96ee8c76695c59c4b97
Basit-dysol <basit@dysols.com>
2025-05-09 17:58:06 +0300

    hover effect on our work section

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -9,20 +9,20 @@ export default function ProductCard({
   return (
     <Link
       href={`/our-work/${slug}`}
-      className="block product-card group w-full rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-300">
+      className="block product-card group w-full rounded-2xl overflow-hidden shadow-md hover:shadow-lg transition-all duration-500">
       <div className="relative w-full h-full rounded-2xl overflow-hidden">
         <img
           src={image}
           alt=""
-          className="w-full h-full object-cover rounded-2xl group-hover:scale-[1.2] transform transition-all duration-500"
+          className="w-full h-full object-cover rounded-2xl group-hover:scale-[1.1] transform transition-all duration-800"
         />
 
-        <div className="absolute inset-0 transition-all duration-300 rounded-2xl" />
+        <div className="absolute inset-0 transition-all duration-500 rounded-2xl" />
 
-        <div className="transform transition-all duration-500 flex flex-col justify-end px-12 pb-12 w-full text-left absolute top-0 left-0 w-full h-full overflow-hidden">
+        <div className="transform transition-all duration-800 flex flex-col justify-end px-12 pb-12 w-full text-left absolute top-0 left-0 h-full overflow-hidden">
           <h2
             className="
-            text-white/90 text-4xl transform transition-all duration-500
+            text-white/90 text-4xl transform transition-all duration-800
           ">
             {description}
           </h2>
