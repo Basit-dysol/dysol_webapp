706038a0fac1ca2e4aa0edd1b21c990171564ff7
Basit-dysol <basit@dysols.com>
2025-05-13 17:33:34 +0300

    changes with logo

--- components/HeroSection/HeroSection.js
+++ components/HeroSection/HeroSection.js
@@ -9,14 +9,14 @@ export default function HeroSection() {
   return (
     <>
       <section className="flex flex-col w-full min-h-[calc(100vh-16px)] text-white justify-end bg-[url('/LandingPage.png')] bg-cover bg-center rounded-4xl pb-16 px-24 xl:aspect-[156/100] 2xl:px-32 space-y-6 pr-0 xl:pr-[40vw] 2xl:pr-[40vw]">
-        <h1 className="text-4xl xl:text-[4.25rem] 2xl:text-[5rem] xl:w-[45vw] 2xl:w-[45vw] text-left font-medium xl:leading-[5rem] 2xl:leading-24">
+        <h1 className="text-4xl xl:text-[4.25rem] 2xl:text-[5rem] xl:w-[45vw] 2xl:w-[50vw] text-left font-medium xl:leading-[5rem] 2xl:leading-24">
           Let's take your
           <br className="hidden sm:block" />
           idea from concept
           <br className="hidden sm:block" />
           to market
         </h1>
-        <p className="xl:text-shadow-md 2xl:text-xl xl:w-[42vw] 2xl:w-[40vw] text-white/60 font-light text-left leading-relaxed xl:leading-[24px]">
+        <p className="xl:text-shadow-md 2xl:text-xl xl:w-[42vw] 2xl:w-[40vw] text-white/60 font-light text-left leading-relaxed xl:leading-8">
           At Dysol, we bring your product idea to life with top-tier engineering
           and design. From idea to manufacturing, we're your trusted partner in
           end-to-end product development.
