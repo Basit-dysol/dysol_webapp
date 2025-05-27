4014be1c8458bb0a654f1ef273810a51e048f492
Basit-dysol <basit@dysols.com>
2025-05-27 22:24:00 +0300

    remove margin

--- components/Container.js
+++ components/Container.js
@@ -1,8 +1,7 @@
 export const Container = ({ className, ...props }) => (
   <div
     {...props}
-    className={`w-full max-w-[1920px] bg-[#1A1A1A] m-1.5  mx-auto ${className || ''}`}
+    className={`w-full max-w-[1920px] bg-[#1A1A1A] mx-auto ${className || ''}`}
     // className={`w-full bg-[#1A1A1A] mx-auto ${className || ''}`}
-
   />
 );
